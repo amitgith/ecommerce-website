14bfb53c94fb20c91afc1c6b392c55c9576a0f88
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-22 14:08:47 +0530

    login functionality add done

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
   const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -18,6 +19,7 @@ const Login = () => {
     const user = users.find(
       (u) => u.email === data.email && u.password === data.password,
     );
+
     if (user) {
       localStorage.setItem("currentUser", JSON.stringify(user));
       navigate("/");
@@ -29,38 +31,127 @@ const Login = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col lg:flex-row bg-[#080808] text-white font-sans">
-      {/* Left Side */}
-      <div className="w-full lg:w-1/2 flex flex-col justify-between p-8 sm:p-12 lg:p-16 relative overflow-hidden">
+    <div
+      className="
+    min-h-screen 
+    flex 
+    flex-col 
+    lg:flex-row 
+    bg-[#080808] 
+    text-white 
+    font-sans
+    "
+    >
+      {/* LEFT SIDE */}
+
+      <div
+        className="
+        w-full
+        lg:w-1/2
+        flex
+        flex-col
+        justify-between
+        p-5
+        sm:p-8
+        md:p-12
+        lg:p-16
+        relative
+        overflow-hidden
+        min-h-[650px]
+        lg:min-h-screen
+        "
+      >
         {/* Logo */}
+
         <div className="flex items-center gap-3">
-          <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-indigo-600">
+          <div
+            className="
+            flex
+            h-10
+            w-10
+            items-center
+            justify-center
+            rounded-xl
+            bg-indigo-600
+            "
+          >
             <ShoppingBag size={20} className="text-black" />
           </div>
 
-          <span className="text-2xl font-bold">SkyMart</span>
+          <span
+            className="
+            text-xl
+            sm:text-2xl
+            font-bold
+            "
+          >
+            SkyMart
+          </span>
         </div>
 
         {/* Content */}
-        <div className="max-w-xl space-y-6 mt-10 lg:mt-0">
-          <span className="text-xs font-semibold tracking-widest text-indigo-400 uppercase">
+
+        <div
+          className="
+          max-w-xl
+          space-y-5
+          sm:space-y-6
+          mt-12
+          lg:mt-0
+          "
+        >
+          <span
+            className="
+            text-xs
+            font-semibold
+            tracking-widest
+            text-indigo-400
+            uppercase
+            "
+          >
             Welcome back
           </span>
 
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold leading-tight">
+          <h1
+            className="
+            text-3xl
+            sm:text-4xl
+            md:text-5xl
+            lg:text-6xl
+            font-extrabold
+            leading-tight
+            "
+          >
             Shop the future.
             <br />
             <span className="text-indigo-600">Today.</span>
           </h1>
 
-          <p className="text-gray-400 text-sm leading-relaxed max-w-md">
+          <p
+            className="
+            text-gray-400
+            text-sm
+            sm:text-base
+            leading-relaxed
+            max-w-md
+            "
+          >
             Thousands of products, lightning-fast delivery, and prices that make
             your wallet happy.
           </p>
         </div>
 
         {/* Stats */}
-        <div className="grid grid-cols-3 gap-3">
+
+        <div
+          className="
+          grid
+          grid-cols-3
+          gap-2
+          sm:gap-4
+          mt-10
+          "
+        >
           {[
             ["20K+", "Products"],
             ["50K+", "Users"],
@@ -69,72 +160,119 @@ const Login = () => {
             <div
               key={index}
               className="
-          border border-white/10 
-          bg-white/5 
-          backdrop-blur-md
-          rounded-xl
-          p-4
-          text-center
-          "
+              border
+              border-white/10
+              bg-white/5
+              backdrop-blur-md
+              rounded-xl
+              p-3
+              sm:p-4
+              text-center
+              "
             >
-              <h3 className="text-lg font-bold text-indigo-600">{item[0]}</h3>
+              <h3
+                className="
+                text-sm
+                sm:text-lg
+                font-bold
+                text-indigo-600
+                "
+              >
+                {item[0]}
+              </h3>
 
-              <p className="text-[10px] text-white uppercase mt-1">{item[1]}</p>
+              <p
+                className="
+                text-[9px]
+                sm:text-[10px]
+                text-white
+                uppercase
+                mt-1
+                "
+              >
+                {item[1]}
+              </p>
             </div>
           ))}
         </div>
       </div>
 
       {/* Divider Line */}
+
       <div
         className="
-  hidden lg:flex
-  items-center
-  justify-center
-  "
+        hidden
+        lg:flex
+        items-center
+        justify-center
+        "
       >
         <div
           className="
-    h-full
-    w-px
-    bg-white
-    "
+          h-full
+          w-px
+          bg-white/20
+          "
         ></div>
       </div>
+      {/* RIGHT SIDE */}
 
-      {/* Right Side */}
       <div
         className="
-  w-full 
-  lg:w-1/2 
-  flex 
-  items-center 
-  justify-center
-  p-6
-  sm:p-10
-  "
+        w-full
+        lg:w-1/2
+        flex
+        items-center
+        justify-center
+        px-5
+        sm:px-8
+        md:px-12
+        lg:px-16
+        py-10
+        "
       >
         <div
           className="
-    w-full
-    max-w-md
-    rounded-3xl
-    border
-    border-white/10
-    bg-white/5
-    backdrop-blur-xl
-    p-8
-    sm:p-10
-    shadow-2xl
-    "
+          w-full
+          max-w-md
+          rounded-3xl
+          border
+          border-white/10
+          bg-white/5
+          backdrop-blur-xl
+          p-6
+          sm:p-8
+          md:p-10
+          shadow-2xl
+          "
         >
-          <h2 className="text-3xl font-bold">Sign in</h2>
+          <h2
+            className="
+            text-2xl
+            sm:text-3xl
+            font-bold
+            "
+          >
+            Sign in
+          </h2>
 
-          <p className="text-sm text-gray-500 mt-2">
+          <p
+            className="
+            text-sm
+            text-gray-500
+            mt-2
+            "
+          >
             Enter your credentials to continue
           </p>
 
-          <form onSubmit={handleSubmit(formSubmit)} className="mt-8 space-y-5">
+          <form
+            onSubmit={handleSubmit(formSubmit)}
+            className="
+            mt-8
+            space-y-5
+            "
+          >
             {/* Email */}
 
             <div>
@@ -142,12 +280,12 @@ const Login = () => {
                 <Mail
                   size={18}
                   className="
-absolute 
-left-4
-top-1/2
--translate-y-1/2
-text-gray-500
-"
+                  absolute
+                  left-4
+                  top-1/2
+                  -translate-y-1/2
+                  text-gray-500
+                  "
                 />
 
                 <input
@@ -157,42 +295,36 @@ text-gray-500
                   type="email"
                   placeholder="Email address"
                   className="
-w-full
-rounded-xl
-bg-black/40
-border
-border-white/10
-py-3.5
-pl-12
-text-sm
-
-outline-none
-
-transition
-
-focus:border-indigo-600
-
-focus:ring-2
-
-focus:ring-indigo-600
-
-"
+                  w-full
+                  rounded-xl
+                  bg-black/40
+                  border
+                  border-white/10
+                  py-3.5
+                  pl-12
+                  text-sm
+                  outline-none
+                  transition
+                  focus:border-indigo-600
+                  focus:ring-2
+                  focus:ring-indigo-600
+                  "
                 />
               </div>
 
               {errors.email && (
                 <p
                   className="
-mt-2
-text-xs
-text-red-400
-bg-red-500/10
-border
-border-red-500/20
-rounded-lg
-px-3
-py-2
-"
+                  mt-2
+                  text-xs
+                  text-red-400
+                  bg-red-500/10
+                  border
+                  border-red-500/20
+                  rounded-lg
+                  px-3
+                  py-2
+                  "
                 >
                   ⚠ {errors.email.message}
                 </p>
@@ -206,12 +338,12 @@ py-2
                 <Lock
                   size={18}
                   className="
-absolute
-left-4
-top-1/2
--translate-y-1/2
-text-gray-500
-"
+                  absolute
+                  left-4
+                  top-1/2
+                  -translate-y-1/2
+                  text-gray-500
+                  "
                 />
 
                 <input
@@ -231,86 +363,85 @@ text-gray-500
                   type="password"
                   placeholder="Password"
                   className="
-w-full
-rounded-xl
-bg-black/40
-border
-border-white/10
-py-3.5
-pl-12
-
-outline-none
-
-transition
-
-focus:border-indigo-600
-
-focus:ring-2
-
-focus:ring-indigo-600
-
-"
+                  w-full
+                  rounded-xl
+                  bg-black/40
+                  border
+                  border-white/10
+                  py-3.5
+                  pl-12
+                  text-sm
+                  outline-none
+                  transition
+                  focus:border-indigo-600
+                  focus:ring-2
+                  focus:ring-indigo-600
+                  "
                 />
               </div>
 
               {errors.password && (
                 <p
                   className="
-mt-2
-text-xs
-text-red-400
-bg-red-500/10
-border
-border-red-500/20
-rounded-lg
-px-3
-py-2
-"
+                  mt-2
+                  text-xs
+                  text-red-400
+                  bg-red-500/10
+                  border
+                  border-red-500/20
+                  rounded-lg
+                  px-3
+                  py-2
+                  "
                 >
                   ⚠ {errors.password.message}
                 </p>
               )}
             </div>
 
+            {/* Button */}
+
             <button
               type="submit"
               className="
-w-full
-mt-3
-py-3.5
-rounded-xl
-bg-indigo-500
-text-white
-font-semibold
-hover:bg-indigo-600
-transition
-active:scale-95
-cursor-pointer
-shadow-lg
-shadow-[indigo-600]/20
-"
+              w-full
+              mt-3
+              py-3.5
+              rounded-xl
+              bg-indigo-500
+              text-white
+              font-semibold
+              hover:bg-indigo-600
+              transition
+              active:scale-95
+              cursor-pointer
+              shadow-lg
+              shadow-[indigo-600]/20
+              "
             >
               Sign in →
             </button>
 
+            {/* Register Link */}
+
             <p
               className="
-text-center
-text-sm
-text-gray-500
-mt-6
-"
+              text-center
+              text-sm
+              text-gray-500
+              mt-6
+              "
             >
               Don't have an account?
               <NavLink
                 to="/register"
                 className="
-ml-2
-text-white
-font-medium
-hover:text-indigo-300
-transition
-"
+                ml-2
+                text-white
+                font-medium
+                hover:text-indigo-300
+                transition
+                "
               >
                 Create one
               </NavLink>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,175 +1,439 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router";
+import React from "react";
+import { NavLink, useNavigate } from "react-router";
 import { User, Mail, Lock, ShoppingBag } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router";
 
 const Register = () => {
   const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm({ mode: "onChange" });
+  } = useForm({
+    mode: "onChange",
+  });
+
   const formSubmit = (data) => {
     const users = JSON.parse(localStorage.getItem("users")) || [];
+
     users.push(data);
+
     localStorage.setItem("users", JSON.stringify(users));
+
     reset();
+
     navigate("/login");
   };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="w-full max-w-md rounded-2xl bg-white shadow-2xl p-8">
-        {/* Logo */}
-        <div className="flex justify-center">
-          <div className="flex h-16 w-16 items-center justify-center rounded-full bg-indigo-600">
-            <ShoppingBag size={30} className="text-white" />
-          </div>
+    <div
+      className="
+    min-h-screen
+    flex
+    flex-col
+    items-center
+    justify-center
+    bg-[#080808]
+    px-4
+    py-10
+    font-sans
+    "
+    >
+      {/* Logo */}
+
+      <div
+        className="
+      flex
+      items-center
+      gap-2
+      mb-8
+      "
+      >
+        <div
+          className="
+        h-10
+        w-10
+        flex
+        items-center
+        justify-center
+        rounded-xl
+        bg-indigo-400
+        "
+        >
+          <ShoppingBag size={22} className="text-black" />
         </div>
 
+        <span
+          className="
+        text-xl
+        sm:text-2xl
+        font-bold
+        text-white
+        "
+        >
+          Sky
+          <span className="text-indigo-400">Mart</span>
+        </span>
+      </div>
+
+      {/* Card */}
+
+      <div
+        className="
+      w-full
+      max-w-md
+      rounded-3xl
+      border
+      border-white/10
+      bg-white/5
+      backdrop-blur-xl
+      shadow-2xl
+      p-6
+      sm:p-8
+      "
+      >
         {/* Heading */}
-        <h1 className="mt-5 text-center text-3xl font-bold text-gray-800">
-          Create Account
+
+        <h1
+          className="
+        text-2xl
+        sm:text-3xl
+        font-bold
+        text-white
+        "
+        >
+          Create account
         </h1>
 
-        <p className="mt-2 text-center text-gray-500">
+        <p
+          className="
+        mt-2
+        text-sm
+        text-gray-500
+        "
+        >
           Join SkyMart and start shopping
         </p>
 
         {/* Form */}
-        <form onSubmit={handleSubmit(formSubmit)} className="mt-8 space-y-5">
-          {/* Full Name */}
-          <div className="relative">
-            <User
-              size={20}
-              className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
-            />
 
-            <input
-              {...register("fullname", {
-                required: "Full Name is Required",
-                minLength: {
-                  value: 4,
-                  message: "Minimum 4 characters are required",
-                },
-                maxLength: {
-                  value: 20,
-                  message: "Maximum 20 characters are required",
-                },
-              })}
-              type="text"
-              placeholder="Full name"
-              className="w-full rounded-lg border border-gray-300 py-3 pl-12 pr-4 text-gray-700 outline-none transition focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200"
-            />
+        <form
+          onSubmit={handleSubmit(formSubmit)}
+          className="
+        mt-8
+        space-y-4
+        "
+        >
+          {/* Full Name */}
+
+          <div>
+            <div
+              className="
+            relative
+            "
+            >
+              <User
+                size={18}
+                className="
+              absolute
+              left-4
+              top-1/2
+              -translate-y-1/2
+              text-gray-500
+              "
+              />
+
+              <input
+                {...register("fullname", {
+                  required: "Full Name is Required",
+
+                  minLength: {
+                    value: 4,
+                    message: "Minimum 4 characters are required",
+                  },
+
+                  maxLength: {
+                    value: 20,
+                    message: "Maximum 20 characters are required",
+                  },
+                })}
+                type="text"
+                placeholder="Full name"
+                className="
+              w-full
+              rounded-xl
+              bg-black/40
+              border
+              border-white/10
+              py-3.5
+              pl-12
+              pr-4
+              text-sm
+              text-white
+              placeholder:text-gray-600
+              outline-none
+              transition
+              focus:border-indigo-400
+              focus:ring-2
+              focus:ring-indigo-400/20
+              "
+              />
+            </div>
 
             {errors.fullname && (
-              <p className="mt-1 text-sm text-red-500">
+              <p
+                className="
+            mt-2
+            text-xs
+            text-red-400
+            "
+              >
                 {errors.fullname.message}
               </p>
             )}
           </div>
-
           {/* Email */}
-          <div className="relative">
-            <Mail
-              size={20}
-              className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
-            />
 
-            <input
-              {...register("email", {
-                required: "Email is Required",
-              })}
-              type="email"
-              placeholder="Email address"
-              className="w-full rounded-lg border border-gray-300 py-3 pl-12 pr-4 text-gray-700 outline-none transition focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200"
-            />
+          <div>
+            <div
+              className="
+            relative
+            "
+            >
+              <Mail
+                size={18}
+                className="
+              absolute
+              left-4
+              top-1/2
+              -translate-y-1/2
+              text-gray-500
+              "
+              />
+
+              <input
+                {...register("email", {
+                  required: "Email is Required",
+                })}
+                type="email"
+                placeholder="Email address"
+                className="
+              w-full
+              rounded-xl
+              bg-black/40
+              border
+              border-white/10
+              py-3.5
+              pl-12
+              pr-4
+              text-sm
+              text-white
+              placeholder:text-gray-600
+              outline-none
+              transition
+              focus:border-indigo-400
+              focus:ring-2
+              focus:ring-indigo-400/20
+              "
+              />
+            </div>
 
             {errors.email && (
-              <p className="mt-1 text-sm text-red-500">
+              <p
+                className="
+            mt-2
+            text-xs
+            text-red-400
+            "
+              >
                 {errors.email.message}
               </p>
             )}
           </div>
 
           {/* Password */}
-          <div className="relative">
-            <Lock
-              size={20}
-              className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
-            />
 
-            <input
-              {...register("password", {
-                required: "Password is Required",
-                minLength: {
-                  value: 6,
-                  message: "Minimum 6 characters are required",
-                },
-                maxLength: {
-                  value: 9,
-                  message: "Maximum 9 characters are required",
-                },
-              })}
-              type="password"
-              placeholder="Password (min 6 chars)"
-              className="w-full rounded-lg border border-gray-300 py-3 pl-12 pr-4 text-gray-700 outline-none transition focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200"
-            />
+          <div>
+            <div
+              className="
+            relative
+            "
+            >
+              <Lock
+                size={18}
+                className="
+              absolute
+              left-4
+              top-1/2
+              -translate-y-1/2
+              text-gray-500
+              "
+              />
+
+              <input
+                {...register("password", {
+                  required: "Password is Required",
+
+                  minLength: {
+                    value: 6,
+                    message: "Minimum 6 characters are required",
+                  },
+
+                  maxLength: {
+                    value: 9,
+                    message: "Maximum 9 characters are required",
+                  },
+                })}
+                type="password"
+                placeholder="Password (min 6 chars)"
+                className="
+              w-full
+              rounded-xl
+              bg-black/40
+              border
+              border-white/10
+              py-3.5
+              pl-12
+              pr-4
+              text-sm
+              text-white
+              placeholder:text-gray-600
+              outline-none
+              transition
+              focus:border-indigo-400
+              focus:ring-2
+              focus:ring-indigo-400/20
+              "
+              />
+            </div>
 
             {errors.password && (
-              <p className="mt-1 text-sm text-red-500">
+              <p
+                className="
+            mt-2
+            text-xs
+            text-red-400
+            "
+              >
                 {errors.password.message}
               </p>
             )}
           </div>
 
           {/* Confirm Password */}
-          <div className="relative">
-            <Lock
-              size={20}
-              className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400"
-            />
 
-            <input
-              {...register("confirmpassword", {
-                required: "Confirm Password is Required",
-                minLength: {
-                  value: 6,
-                  message: "Minimum 6 characters are required",
-                },
-                maxLength: {
-                  value: 9,
-                  message: "Maximum 9 characters are required",
-                },
-              })}
-              type="password"
-              placeholder="Confirm password"
-              className="w-full rounded-lg border border-gray-300 py-3 pl-12 pr-4 text-gray-700 outline-none transition focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200"
-            />
+          <div>
+            <div
+              className="
+            relative
+            "
+            >
+              <Lock
+                size={18}
+                className="
+              absolute
+              left-4
+              top-1/2
+              -translate-y-1/2
+              text-gray-500
+              "
+              />
+
+              <input
+                {...register("confirmpassword", {
+                  required: "Confirm Password is Required",
+
+                  minLength: {
+                    value: 6,
+                    message: "Minimum 6 characters are required",
+                  },
+
+                  maxLength: {
+                    value: 9,
+                    message: "Maximum 9 characters are required",
+                  },
+                })}
+                type="password"
+                placeholder="Confirm password"
+                className="
+              w-full
+              rounded-xl
+              bg-black/40
+              border
+              border-white/10
+              py-3.5
+              pl-12
+              pr-4
+              text-sm
+              text-white
+              placeholder:text-gray-600
+              outline-none
+              transition
+              focus:border-indigo-400
+              focus:ring-2
+              focus:ring-indigo-400/20
+              "
+              />
+            </div>
 
             {errors.confirmpassword && (
-              <p className="mt-1 text-sm text-red-500">
+              <p
+                className="
+            mt-2
+            text-xs
+            text-red-400
+            "
+              >
                 {errors.confirmpassword.message}
               </p>
             )}
           </div>
 
           {/* Button */}
+
           <button
             type="submit"
-            className="w-full cursor-pointer rounded-lg bg-indigo-600 py-3 font-semibold text-white transition hover:bg-indigo-700"
+            className="
+          w-full
+          mt-3
+          rounded-xl
+          bg-indigo-400
+          py-3.5
+          text-black
+          font-semibold
+          text-sm
+          transition
+          hover:bg-indigo-300
+          active:scale-95
+          cursor-pointer
+          "
           >
             Create Account
+            <span className="ml-2">→</span>
           </button>
 
           {/* Login Link */}
-          <p className="text-center text-sm text-gray-600">
-            Already have an account?{" "}
+
+          <p
+            className="
+          text-center
+          text-sm
+          text-gray-500
+          mt-6
+          "
+          >
+            Already have an account?
             <NavLink
-              className="font-semibold text-indigo-600 hover:underline"
-              to={"/login"}
+              to="/login"
+              className="
+            ml-1
+            text-indigo-400
+            font-semibold
+            hover:text-indigo-300
+            transition
+            "
             >
               Sign in
             </NavLink>
